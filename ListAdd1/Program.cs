aaa96a98d0bbbbe930750befedcd652a25524af2
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-27 19:00:57 +0530

    Add project files.

---
+++ ListAdd1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProductList
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("Rahul");
+            list.Add("Akshay");
+            list.Add("Siddhesh");
+            list.Remove("Siddhesh");
+            foreach (var l in list)
+            {
+                Console.WriteLine(l);
+            }
+            //string[] names = new string[1];
+            // list.Insert(1, "Siddhesh");
+            //list.CopyTo(names);
+            //foreach (var l in list)
+            //{
+            //    Console.WriteLine(l);
+            //}
+            list.Reverse();
+            list.Add("Dnyaneshwar");
+            list.RemoveAt(1);
+            list.IndexOf("Siddhesh");
+            {
+                Console.WriteLine();
+            }
+
+
+
+
+        }
+
+        }
+
+
+}
